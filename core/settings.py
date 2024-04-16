4d3b141cde7c885e49af8cab936804f00812fb1c
dmm <workspace99@gmail.com>
2024-04-16 18:13:20 +0200

    update lang and remove bootstrap5 in settings.py

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
     'accounts.apps.AccountsConfig',
     'crm.apps.CrmConfig',
 ]
