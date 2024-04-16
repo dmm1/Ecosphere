6c8aae33cea0c39e36f5a542a8bd52a7e3253a2e
dmm <workspace99@gmail.com>
2024-04-17 00:07:14 +0200

    adding phone to customer

--- crm/forms.py
+++ crm/forms.py
@@ -14,4 +14,4 @@ class OpportunityForm(forms.ModelForm):
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = ['name', 'email']
+        fields = ['name', 'email', 'phone']
