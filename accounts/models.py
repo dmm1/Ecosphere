c35b230dc049f0b233697d93a6efa577290d80c3
dmm <workspace99@gmail.com>
2024-04-17 11:13:08 +0200

    update of the user profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,30 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    forename = models.CharField(max_length=30, blank=True)
+    surname = models.CharField(max_length=30, blank=True)
+    email_address = models.EmailField(blank=True)
     bio = models.TextField(blank=True, null=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures', null=True, blank=True)
 
+    def save(self, *args, **kwargs):
+        self.forename = self.user.first_name
+        self.surname = self.user.last_name
+        self.email_address = self.user.email
+        super(UserProfile, self).save(*args, **kwargs)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 # Create your models here.
