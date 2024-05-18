0e45f3a3b33f94393deab62e267aedf48e6ba779
dmm <workspace99@gmail.com>
2024-05-19 00:15:46 +0200

    layout fix, update seeds fro contacts and businesspartner for correct values, update requirements.txt

--- apps/business_partner/management/commands/seed_businesspartners.py
+++ apps/business_partner/management/commands/seed_businesspartners.py
@@ -11,9 +11,10 @@ class Command(BaseCommand):
         self.faker = Faker()
 
     def handle(self, *args, **kwargs):
+        users = list(User.objects.all())
         for i in range(20):  # Change the range to the number of instances you want to create
             business_partner_data = self.generate_fake_data(BusinessPartner)
-            business_partner_data['user'] = User.objects.first() if User.objects.exists() else None
+            business_partner_data['user'] = self.faker.random_element(users) if users else None
             BusinessPartner.objects.create(**business_partner_data)
 
         self.stdout.write(self.style.SUCCESS('Successfully seeded the database with BusinessPartner instances'))
@@ -24,12 +25,27 @@ class Command(BaseCommand):
             if field.name in ['id', 'created_at', 'updated_at']:
                 continue
             elif field.get_internal_type() == 'CharField':
-                data[field.name] = self.faker.word()
-            elif field.get_internal_type() == 'TextField':
-                data[field.name] = self.faker.text()
+                if field.name == 'email':
+                    data[field.name] = self.faker.email()
+                elif field.name == 'phone':
+                    data[field.name] = '+4366555' + str(self.faker.random_int(min=10, max=99))  # Generate phone number in the format +436655588
+                elif field.name in ['industry', 'primary_role', 'secondary_role']:
+                    if field.choices is not None:
+                        data[field.name] = self.faker.random_element(elements=[choice[0] for choice in field.choices])
+                    # Add an else clause here if you want to handle the case where field.choices is None
+                elif field.name in ['country', 'state', 'city']:
+                    data[field.name] = self.faker.word()
+                elif field.name in ['postcode', 'street_number']:
+                    data[field.name] = self.faker.random_int(min=1000, max=9999)
+                elif field.name == 'street_name':
+                    data[field.name] = self.faker.street_name()
+                else:
+                    data[field.name] = self.faker.word()
+            elif field.get_internal_type() == 'EmailField':
+                data[field.name] = self.faker.email()
             elif field.get_internal_type() == 'BooleanField':
                 data[field.name] = self.faker.boolean()
             elif field.get_internal_type() == 'DateField':
-                data[field.name] = self.faker.date()
+                data[field.name] = self.faker.date_between(start_date='-30y', end_date='today')
             # Add more elif conditions here for other field types as needed
         return data
--- apps/contact/management/commands/seed_contacts.py
+++ apps/contact/management/commands/seed_contacts.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from apps.contact.models import Contact
 from apps.business_partner.models import BusinessPartner
+from django.contrib.auth.models import User
 from faker import Faker
 
 class Command(BaseCommand):
@@ -11,20 +12,23 @@ class Command(BaseCommand):
         self.faker = Faker()
 
     def handle(self, *args, **kwargs):
+        business_partners = list(BusinessPartner.objects.all())
+        users = list(User.objects.all())
         for i in range(20):  # Change the range to the number of instances you want to create
             contact_data = self.generate_fake_data(Contact)
-            contact_data['business_partner'] = BusinessPartner.objects.first() if BusinessPartner.objects.exists() else None
+            contact_data['business_partner'] = self.faker.random_element(business_partners) if business_partners else None
+            contact_data['user'] = self.faker.random_element(users) if users else None
             contact, created = Contact.objects.get_or_create(
                 first_name=self.faker.first_name(),
                 last_name=self.faker.last_name(),
                 defaults=contact_data
             )
-
+    
             if not created:
                 for key, value in contact_data.items():
                     setattr(contact, key, value)
                 contact.save()
-
+    
         self.stdout.write(self.style.SUCCESS('Successfully seeded Contact instances'))
 
     def generate_fake_data(self, model):
@@ -33,12 +37,26 @@ class Command(BaseCommand):
             if field.name in ['id', 'created_at', 'updated_at']:
                 continue
             elif field.get_internal_type() == 'CharField':
-                data[field.name] = self.faker.word()
+                if field.name == 'email':
+                    data[field.name] = self.faker.email()
+                elif field.name == 'phone':
+                    data[field.name] = '+4366555' + str(self.faker.random_int(min=10, max=99))  # Generate phone number in the format +436655588
+                elif field.name in ['title', 'department', 'preferred_communication']:
+                    data[field.name] = self.faker.random_element(elements=[choice[0] for choice in field.choices])
+                else:
+                    data[field.name] = self.faker.word()
             elif field.get_internal_type() == 'TextField':
                 data[field.name] = self.faker.text()
             elif field.get_internal_type() == 'BooleanField':
                 data[field.name] = self.faker.boolean()
             elif field.get_internal_type() == 'DateField':
-                data[field.name] = self.faker.date()
+                data[field.name] = self.faker.date_between(start_date='-30y', end_date='today')
+            elif field.get_internal_type() == 'ForeignKey':
+                if field.name == 'business_partner':
+                    if BusinessPartner.objects.exists():
+                        data[field.name] = BusinessPartner.objects.order_by('?').first()
+                elif field.name == 'user':
+                    if User.objects.exists():
+                        data[field.name] = User.objects.order_by('?').first()
             # Add more elif conditions here for other field types as needed
         return data
