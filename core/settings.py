93231d1beb32fb2e1dd3a2060afe745ae4b33a5a
dmm <workspace99@gmail.com>
2024-05-27 22:59:57 +0200

    fix .env

--- core/settings.py
+++ core/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from datetime import timedelta
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
